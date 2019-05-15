99ff65b7cad7a0f325b8163646111e0037efecbf
Katrina Nickel <katrina.querubin@gmail.com>
2019-05-15 16:26:01 -0400

    Final version: lbs converter

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// 1. When a user hovers on the entry box, a scroll tab appears on the right (remove negative number)
-// 2. the user can enter their own number
-// 3. Once the number is entered, there will be a submit button on the right side
-// 4. The submit button is selected
-// 5. Once the submit button is selected, the data will be retrieved 
-// 6. Then convert the lbs entered into grams, kilograms or ounces 
-// 7. Then show 
+//1. Display an empty box to input lbs
+//2. User enters lbs
+//3. Once user enters lbs, the conversion for grams, oz and kilograms are displayed
 
-
-//Version 5 with entering lbs: CORRECT VERSION.  USE THIS AS BACKUP
 $('#output').css('visibility', 'hidden');
 $('#lbsInput').keyup(function () {
   $('#output').css('visibility', 'visible');
