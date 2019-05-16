8b61890d46339e45c13fd8ff0f549c32c39d8560
Katrina Nickel <katrina.querubin@gmail.com>
2019-05-15 20:52:45 -0400

    V1: Revised html and removed JS code

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 //2. User enters lbs
 //3. Once user enters lbs, the conversion for grams, oz and kilograms are displayed
 
-$('#output').css('visibility', 'hidden');
-$('#lbsInput').keyup(function () {
-  $('#output').css('visibility', 'visible');
-  const lbs = $('#lbsInput').val();
-  $('#gramsOutput').html(Math.floor(lbs * 453.59237));
-  $('#kgOutput').html((lbs * 0.45359237).toFixed(2));
-  $('#ozOutput').html(Math.floor(lbs * 16));
-});
+// $('#output').css('visibility', 'hidden');
+// $('#lbsInput').keyup(function () {
+//   $('#output').css('visibility', 'visible');
+//   const lbs = $('#lbsInput').val();
+//   $('#gramsOutput').html(Math.floor(lbs * 453.59237));
+//   $('#kgOutput').html((lbs * 0.45359237).toFixed(2));
+//   $('#ozOutput').html(Math.floor(lbs * 16));
+// });
 
 
 
